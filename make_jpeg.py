5366dc2e69d0d0808c7acd3611dd2652337d43a8
Matthew Bourque <bourque@stsci.edu>
2017-07-21 10:18:39 -0400

    Initial commit of make_jpeg.

---
+++ make_jpeg.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+
+"""Create a JPEG file from a RAW, FLT, or FLC FITS file from the ACS
+or WFC3 instrument.  Currently, other instruments (e.g. STIS) and other
+filetyles (e.g. IMA) are not not supported, but likely the code could
+be easily extended to support these.
+
+Authors
+-------
+
+    Matthew Bourque
+    Alex Viana
+
+Use
+---
+
+    This module is intended to be used via the python environemnt, for
+    example:
+
+        import make_jpeg
+        make_jpeg(<path_to_file>)
+
+Dependencies
+------------
+
+    This module is supported in both Python 2.7 and Python 3+.  This
+    module also depends on astropy, numpy, and the Python Image Library
+    (PIL), also known as 'Pillow'.  These can be installed via 'conda'
+    or 'pip':
+
+    conda/pip install astropy
+    conda/pip install numpy
+    conda/pip install Pillow
+"""
+
+import copy
+
+from astropy.io import fits
+import numpy as np
+from PIL import Image
+
+
+def make_jpeg(filename):
+    """Create a JPEG file from a raw, flt, or flc FITS file.  If the
+    image is a full-frame WFC/UVIS or ACS/WFC image, the data from the
+    1st and 4th extensions will be combined into one JPEG.
+
+    Parameters
+    ----------
+    filename : str
+        The path to the file.
+    """
+
+    with fits.open(filename) as hdulist:
+
+        # Get the image data.
+        data = hdulist[1].data
+
+        # For full UVIS or WFC image.
+        if (hdulist[0].header['DETECTOR'] in ['UVIS', 'WFC']
+        and len(hdulist) > 4
+        and hdulist[4].header['EXTNAME'] == 'SCI'):
+            data2 = hdulist[4].data
+            height = data.shape[0] + data2.shape[0]
+            width = data.shape[1]
+            temp = np.zeros((height, width))
+            temp[0:int(height/2), :] = data
+            temp[int(height/2):height, :] = data2
+            data = temp
+
+    # Clip the top and bottom 1% of pixels.
+    sorted_data = copy.copy(data)
+    sorted_data = sorted_data.ravel()
+    sorted_data.sort()
+    top = sorted_data[int(len(sorted_data) * 0.99)]
+    bottom = sorted_data[int(len(sorted_data) * 0.01)]
+    top_index = np.where(data > top)
+    data[top_index] = top
+    bottom_index = np.where(data < bottom)
+    data[bottom_index] = bottom
+
+    # Scale the data.
+    data = data - data.min()
+    data = (data / data.max()) * 255.
+    data = np.flipud(data)
+    data = np.uint8(data)
+
+    # Write the image to a JPEG
+    image = Image.fromarray(data)
+    image.save(filename.replace('.fits', '.jpg'))
